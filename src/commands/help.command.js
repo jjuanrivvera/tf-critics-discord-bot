8bc0b438033a853dd1ac9aff406272a2e0ad697b
jjuanrivvera99 <juan.rivera@alegra.com>
2021-05-05 21:28:07 -0500

    Help command modified

--- src/commands/help.command.js
+++ src/commands/help.command.js
@@ -1,27 +1,32 @@
 const { MessageEmbed } = require('discord.js');
 const Command = require('../models/command.model');
 
+const getCommandsEmbed = async (group = "User") => {
+    const commands = await Command.find({ group: group});
+
+    let commandEmbed = [];
+    let help = new MessageEmbed().setColor("0x1D82B6");
+    help.setTitle("Available Command List");
+
+    commands.forEach(function(command) {
+        help.addField(`${command.name}`, `**Description:** ${command.description}\n**Usage:** ${command.usage}`);
+        commandEmbed.push(command);
+    });
+
+    return help;
+};
+
 module.exports.run = async (message, args) => {
-    try {
-        if (!args.length) {
-            await Command.find({}, async function(err, commands) {
-                let commandMap = [];
-                let help = new MessageEmbed().setColor("0x1D82B6");
-                help.setTitle("Available Command List");
+    if (args.length <= 0) {
+        return await message.channel.send(await getCommandsEmbed());
+    } else {
+        const group = args[0];
 
-                commands.forEach(function(command) {
-                    if (command.group === "User") {
-                        help.addField(`${command.name}`, `**Description:** ${command.description}\n**Usage:** ${command.usage}`);
-                    }
-                    commandMap.push(command);
-                });
-
-                message.channel.send(help);
-            });
+        if (!group) {
+            return await message.channel.send(await getCommandsEmbed());
         }
-    } catch (err) {
-        console.log(err);
-        message.channel.send("Error displaying help menu").then(msg => msg.delete({ timeout: 3000 }));
+
+        await message.channel.send(await getCommandsEmbed(group));
     }
 }
 
