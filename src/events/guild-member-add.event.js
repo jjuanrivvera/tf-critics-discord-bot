61f5dfc8b47c3eccd3f2df35c6095eb95e49fe0c
jjuanrivvera99 <juan.rivera@alegra.com>
2021-06-03 10:07:52 -0500

    Rank card modified

--- src/events/guild-member-add.event.js
+++ src/events/guild-member-add.event.js
@@ -9,23 +9,17 @@ module.exports = {
         const guildModel = await Guild.findOne({
             id: guild.id
         });
+        
+        redis.client.get(`muted-${id}`, async (err, result) => {
+            if (err) {
+                console.log("Redis error", err);
+            } else if (result) {
+                const role = await guild.roles.cache.find(role => role.id === guildModel.mutedRoleId);
 
-        const redisClient = await redis();
-
-        try {
-            redisClient.get(`muted-${id}`, async (err, result) => {
-                if (err) {
-                    console.log("Redis error", err);
-                } else if (result) {
-                    const role = await guild.roles.cache.find(role => role.id === guildModel.mutedRoleId);
-
-                    if (role) {
-                        await member.roles.add(role);
-                    }
+                if (role) {
+                    await member.roles.add(role);
                 }
-            });
-        } finally {
-            redisClient.quit();
-        }
+            }
+        });
 	}
 };
